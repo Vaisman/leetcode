bb10578e4b3abb7ba444f3ad4e43b75c13d099b1
Vasili Svirski <>
2018-02-26 11:11:43 +0300

    Find Largest Value in Each Tree Row

--- src/main/java/Solution1.java
+++ src/main/java/Solution1.java
@@ -7,16 +7,9 @@ import java.util.Queue;
 
 import static org.junit.Assert.assertArrayEquals;
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int x) { val = x; }
-}
+public class Solution1 {
 
-public class Solution {
-
-    public List<Integer> largestValues(TreeNode root) {
+    public List<Integer> largestValuesBFS(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         if (root == null) return result;
 
@@ -39,6 +32,28 @@ public class Solution {
         return result;
     }
 
+    public List<Integer> largestValuesDFS(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res, 0);
+
+        return res;
+    }
+
+    private void helper(TreeNode root, List<Integer> res, int d){
+        if (root == null){
+            return;
+        }
+
+        if (d == res.size()){
+            res.add(root.val);
+        } else if (res.get(d) < root.val) {
+            res.set(d, root.val);
+        }
+
+        helper(root.left, res, d+1);
+        helper(root.right, res, d+1);
+    }
+
     @Test
     public void test() {
         TreeNode root = new TreeNode(1);
@@ -50,9 +65,17 @@ public class Solution {
 
         root.right.right = new TreeNode(9);
 
-        List<Integer> resultList  = new Solution().largestValues(root);
+        List<Integer> resultList  = new Solution1().largestValuesBFS(root);
+        List<Integer> resultListRec  = new Solution1().largestValuesDFS(root);
+
         Integer[] result = new Integer[resultList.size()];
+
+        Integer[] testData = new Integer[]{1, 3, 9};
+
         resultList.toArray(result);
-        assertArrayEquals(new Integer[]{1, 3, 9},result);
+        assertArrayEquals(testData, result);
+
+        resultListRec.toArray(result);
+        assertArrayEquals(testData, result);
     }
 }
