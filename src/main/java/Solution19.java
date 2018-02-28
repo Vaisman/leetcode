ab2c3011fa161c150cc221db5702faf244659ae7
Vasili Svirski <vasili.svirski@gmail.com>
2018-02-28 10:33:52 +0300

    Minimum Depth of Binary Tree

---
+++ src/main/java/Solution19.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Solution19 {
+
+    public int minDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        if (root.left == null && root.right == null)
+            return 1;
+
+        if (root.left == null)
+          return minDepth(root.right) + 1;
+
+        if (root.right == null)
+          return minDepth(root.left) + 1;
+
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+
+    @Test
+    public void test() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        assertEquals(2, minDepth(root));
+    }
+
+    @Test
+    public void testEmpty() {
+        assertEquals(0, minDepth(null));
+    }
+}
