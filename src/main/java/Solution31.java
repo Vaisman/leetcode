468205cb14d5d0604c3326f0ece6213bc2a43e7a
Vasili Svirski <vsvirski@gmail.com>
2018-03-07 10:35:48 +0300

    Nim Game

---
+++ src/main/java/Solution31.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class Solution31 {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0 ;
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(false, canWinNim(0));
+        assertEquals(true, canWinNim(1));
+    }
+}
