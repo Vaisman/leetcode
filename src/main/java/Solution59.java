cb5b2cc552647c36961f2dad490a43817c4517c6
Vasili Svirski <vsvirski@gmail.com>
2018-03-20 08:05:07 -0700

    Verify Preorder Serialization of a Binary Tree

---
+++ src/main/java/Solution59.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Solution59 {
+  public boolean isValidSerialization(String preorder) {
+    if (preorder == null || preorder.isEmpty()) return false;
+
+    String[] nodes = preorder.split(",");
+    int diff = 1;
+
+    for (String node : nodes) {
+      diff -= 1;
+
+      if (diff < 0) {
+        return false;
+      }
+
+      if (!node.equals("#")) {
+        diff += 2;
+      }
+    }
+
+    return diff == 0;
+  }
+
+  @Test
+  public void test() {
+    assertEquals(true, isValidSerialization("9,3,4,#,#,1,#,#,2,#,6,#,#"));
+  }
+}
