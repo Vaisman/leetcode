479faf69cf7686b44ec9fc36af903d972aee7fae
Vasili Svirski <vsvirski@gmail.com>
2018-03-23 16:02:10 +0300

    Contains Duplicate II

--- src/main/java/Solution82.java
+++ src/main/java/Solution82.java
@@ -1,7 +1,9 @@
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 
@@ -20,8 +22,26 @@ public class Solution82 {
     return false;
   }
 
+  public boolean containsNearbyDuplicate2(int[] nums, int k) {
+    if (nums == null || nums.length == 0) return false;
+
+    Set<Integer> set = new HashSet<>(k + 1);
+    for (int i = 0; i < nums.length; i++) {
+      if (set.contains(nums[i])) {
+        return true;
+      }
+      set.add(nums[i]);
+      if (set.size() > k) {
+        set.remove(nums[i - k]);
+      }
+    }
+
+    return false;
+  }
+
   @Test
   public void test() {
     assertEquals(containsNearbyDuplicate(new int[] {-1, -1}, 1), true);
+    assertEquals(containsNearbyDuplicate2(new int[] {-1, -1}, 1), true);
   }
 }
