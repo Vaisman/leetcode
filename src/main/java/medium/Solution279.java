c76b32b92e89e02c22ae2a02931f5292932ea6e6
Vasili Svirski <vasili.svirski@gmail.com>
2018-05-10 10:52:23 +0300

    Perfect Squares

---
+++ src/main/java/medium/Solution279.java
@@ -0,0 +1,28 @@
+package medium;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class Solution279 {
+  public int numSquares(int n) {
+    int[] dp = new int[n + 1];
+    Arrays.fill(dp, Integer.MAX_VALUE);
+    dp[0] = 0;
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j * j <= i; j++) {
+        dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+      }
+    }
+
+    return dp[n];
+  }
+
+  @Test
+  public void test() {
+    assertEquals(numSquares(12), 3);
+  }
+}
