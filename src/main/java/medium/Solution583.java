5039b87a5ee9a9cfa99fb59e0d13626712b22d46
Vasili Svirski <vasili.svirski@gmail.com>
2018-06-06 11:31:57 +0300

    Delete Operation for Two Strings

--- src/main/java/medium/Solution583.java
+++ src/main/java/medium/Solution583.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class Solution583 {
-  public int minDistance(String s1, String s2) {
+  public int minDistance1(String s1, String s2) {
     int[][] memo = new int[s1.length() + 1][s2.length() + 1];
     return s1.length() + s2.length() - 2 * lcs(s1, s2, s1.length(), s2.length(), memo);
   }
@@ -16,16 +16,27 @@ public class Solution583 {
     }
 
     if (memo[m][n] != 0) {
-        return memo[m][n];
+      return memo[m][n];
     }
-
 
     if (s1.charAt(m - 1) == s2.charAt(n - 1)) {
-        memo[m][n] = 1 + lcs(s1, s2, m - 1, n - 1, memo);
+      memo[m][n] = 1 + lcs(s1, s2, m - 1, n - 1, memo);
     } else {
-        memo[m][n] = Math.max(lcs(s1, s2, m, n - 1, memo), lcs(s1, s2, m - 1, n, memo));
+      memo[m][n] = Math.max(lcs(s1, s2, m, n - 1, memo), lcs(s1, s2, m - 1, n, memo));
     }
     return memo[m][n];
+  }
+
+  public int minDistance(String s1, String s2) {
+    int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+    for (int i = 0; i <= s1.length(); i++) {
+      for (int j = 0; j <= s2.length(); j++) {
+        if (i == 0 || j == 0) continue;
+        if (s1.charAt(i - 1) == s2.charAt(j - 1)) dp[i][j] = 1 + dp[i - 1][j - 1];
+        else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+    return s1.length() + s2.length() - 2 * dp[s1.length()][s2.length()];
   }
 
   @Test
