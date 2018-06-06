de39f975e5b76f8fd61e1e00b37e69c1781b9ddf
Vasili Svirski <vasili.svirski@gmail.com>
2018-06-06 11:20:22 +0300

    Delete Operation for Two Strings

--- src/main/java/medium/Solution583.java
+++ src/main/java/medium/Solution583.java
@@ -6,19 +6,26 @@ import static org.junit.Assert.assertEquals;
 
 public class Solution583 {
   public int minDistance(String s1, String s2) {
-    return s1.length() + s2.length() - 2 * lcs(s1, s2, s1.length(), s2.length());
+    int[][] memo = new int[s1.length() + 1][s2.length() + 1];
+    return s1.length() + s2.length() - 2 * lcs(s1, s2, s1.length(), s2.length(), memo);
   }
 
-  int lcs(String s1, String s2, int m, int n) {
+  int lcs(String s1, String s2, int m, int n, int[][] memo) {
     if (m == 0 || n == 0) {
       return 0;
     }
 
-    if (s1.charAt(m - 1) == s2.charAt(n - 1)) {
-      return 1 + lcs(s1, s2, m - 1, n - 1);
-    } else {
-      return Math.max(lcs(s1, s2, m, n - 1), lcs(s1, s2, m - 1, n));
+    if (memo[m][n] != 0) {
+        return memo[m][n];
     }
+
+
+    if (s1.charAt(m - 1) == s2.charAt(n - 1)) {
+        memo[m][n] = 1 + lcs(s1, s2, m - 1, n - 1, memo);
+    } else {
+        memo[m][n] = Math.max(lcs(s1, s2, m, n - 1, memo), lcs(s1, s2, m - 1, n, memo));
+    }
+    return memo[m][n];
   }
 
   @Test
