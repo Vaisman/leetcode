84321f4f97b05f032fd0912d90024977df41572b
Vasili Svirski <vasili.svirski@gmail.com>
2018-06-20 11:47:34 +0300

    License Key Formatting

---
+++ src/main/java/easy/Solution482.java
@@ -0,0 +1,15 @@
+package easy;
+
+public class Solution482 {
+  public String licenseKeyFormatting(String s, int k) {
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = s.length() - 1; i >= 0; i--) {
+      if (s.charAt(i) == '-') continue;
+
+      sb.append(sb.length() % (k + 1) == k ? '-' : "").append(s.charAt(i));
+    }
+
+    return sb.reverse().toString().toUpperCase();
+  }
+}
