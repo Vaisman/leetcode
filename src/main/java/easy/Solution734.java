28afc480096cd7950c61b99ec8a8e8cef90d8309
Vasili Svirski <vasili.svirski@gmail.com>
2018-06-22 10:32:25 +0300

    Sentence Similarity

--- src/main/java/easy/Solution734.java
+++ src/main/java/easy/Solution734.java
@@ -1,24 +1,31 @@
 package easy;
 
+import org.junit.Test;
+
 import java.util.HashSet;
 import java.util.Set;
 
 public class Solution734 {
   public boolean areSentencesSimilar(String[] words1, String[] words2, String[][] pairs) {
-      // check on null and empty and invalid data
+    // check on null and empty and invalid data
 
-      if (words1.length != words2.length) return false;
+    if (words1.length != words2.length) return false;
 
-      Set<String> set = new HashSet<>();
-      for(String[] pair: pairs){
-          set.add(pair[0]+"#"+pair[i]);
-      }
-      for(int i = 0;i<words1.length;i++) {
-          if (words1[i].equals(words2[i])) continue;
-          if (set.contains(words1[i]+"#"+words2[i]) || set.contains(words2[i]+"#"+words1[i]))  continue;
-          return false;
+    Set<String> set = new HashSet<>();
+    for (String[] pair : pairs) {
+      set.add(pair[0] + "#" + pair[0]);
+    }
+    for (int i = 0; i < words1.length; i++) {
+      if (words1[i].equals(words2[i])) continue;
+      if (set.contains(words1[i] + "#" + words2[i]) || set.contains(words2[i] + "#" + words1[i]))
+        continue;
+      return false;
+    }
+    return true;
+  }
 
-      }
-      return true;
+  @Test
+  public void test() {
+    areSentencesSimilar(new String[] {""}, new String[] {""}, new String[][] {{""}});
   }
 }
