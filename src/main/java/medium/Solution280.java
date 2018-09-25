7aa5c6bb158d487e8e790b6648663c37336dbd33
Vasili Svirski <vasili.svirski@gmail.com>
2018-09-25 18:12:18 +0300

    Wiggle Sort

---
+++ src/main/java/medium/Solution280.java
@@ -0,0 +1,18 @@
+package medium;
+
+import java.util.Arrays;
+
+public class Solution280 {
+  public void wiggleSort(int[] nums) {
+    Arrays.sort(nums);
+    for (int i = 1; i < nums.length - 1; i += 2) {
+      swap(nums, i, i + 1);
+    }
+  }
+
+  private void swap(int[] nums, int i, int j) {
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+  }
+}
