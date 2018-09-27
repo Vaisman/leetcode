5f87ea409123e2758e0d30d4438bf360a5751893
Vasili Svirski <vasili.svirski@gmail.com>
2018-09-27 12:18:08 +0300

    Hand of Straights

---
+++ src/main/java/medium/Solution846.java
@@ -0,0 +1,23 @@
+package medium;
+
+import java.util.TreeMap;
+import java.util.Map;
+
+public class Solution846 {
+  public boolean isNStraightHand(int[] hand, int W) {
+    Map<Integer, Integer> c = new TreeMap<>();
+    for (int i : hand) {
+      c.put(i, c.getOrDefault(i, 0) + 1);
+    }
+
+    for (int it : c.keySet()) {
+      if (c.get(it) > 0)
+        for (int i = W - 1; i >= 0; --i) {
+          if (c.getOrDefault(it + i, 0) < c.get(it)) return false;
+          c.put(it + i, c.get(it + i) - c.get(it));
+        }
+    }
+
+    return true;
+  }
+}
