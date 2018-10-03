bc6036c3349272f89c2c6abdd3de4388536ccf68
Vasili Svirski <vasili.svirski@gmail.com>
2018-10-03 18:20:58 +0300

    Random Pick with Weight

---
+++ src/main/java/medium/Solution528.java
@@ -0,0 +1,32 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Solution528 {
+
+  List<Integer> psum = new ArrayList<>();
+  int tot = 0;
+  Random rand = new Random();
+
+  public Solution528(int[] w) {
+    for (int x : w) {
+      tot += x;
+      psum.add(tot);
+    }
+  }
+
+  public int pickIndex() {
+    int targ = rand.nextInt(tot);
+
+    int lo = 0;
+    int hi = psum.size() - 1;
+    while (lo != hi) {
+      int mid = (lo + hi) / 2;
+      if (targ >= psum.get(mid)) lo = mid + 1;
+      else hi = mid;
+    }
+    return lo;
+  }
+}
