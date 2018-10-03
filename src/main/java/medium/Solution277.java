01297d5a59805be4587bf1ba810604260667f081
Vasili Svirski <vasili.svirski@gmail.com>
2018-10-03 16:04:22 +0300

    Find the Celebrity

---
+++ src/main/java/medium/Solution277.java
@@ -0,0 +1,23 @@
+package medium;
+
+public class Solution277 {
+  boolean knows(int i, int j) {
+    return true;
+  }
+
+  public int findCelebrity(int n) {
+    int candidate = 0;
+
+    for (int i = 1; i < n; i++) {
+      if (knows(candidate, i)) candidate = i;
+    }
+
+    for (int i = 0; i < n; i++) {
+      if (i != candidate &&
+              (knows(candidate, i) || !knows(i, candidate)))
+          return -1;
+    }
+
+    return candidate;
+  }
+}
