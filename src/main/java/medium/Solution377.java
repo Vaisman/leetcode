967b1cd3d33663389b0736cfcb1f3d55e5c34dfe
Vasili Svirski <vasili.svirski@gmail.com>
2018-10-09 16:48:12 +0300

    Combination Sum IV

---
+++ src/main/java/medium/Solution377.java
@@ -0,0 +1,20 @@
+package medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution377 {
+  Map<Integer, Integer> map = new HashMap<>();
+
+  public int combinationSum4(int[] nums, int target) {
+    int count = 0;
+    if (nums == null || nums.length == 0 || target < 0) return 0;
+    if (target == 0) return 1;
+    if (map.containsKey(target)) return map.get(target);
+    for (int num : nums) {
+      count += combinationSum4(nums, target - num);
+    }
+    map.put(target, count);
+    return count;
+  }
+}
