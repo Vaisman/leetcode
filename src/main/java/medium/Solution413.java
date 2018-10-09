0df9ac532e3e7800bbc23abc41251afa7e8c5e4c
Vasili Svirski <vasili.svirski@gmail.com>
2018-10-09 14:42:31 +0300

    Arithmetic Slices

---
+++ src/main/java/medium/Solution413.java
@@ -0,0 +1,20 @@
+package medium;
+
+public class Solution413 {
+    int sum = 0;
+    public int numberOfArithmeticSlices(int[] A) {
+        slices(A, A.length - 1);
+        return sum;
+    }
+    public int slices(int[] A, int i) {
+        if (i < 2)
+            return 0;
+        int ap = 0;
+        if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+            ap = 1 + slices(A, i - 1);
+            sum += ap;
+        } else
+            slices(A, i - 1);
+        return ap;
+    }
+}
