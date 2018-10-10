080b6e41fba8fbe0f95ace108e1b0b1a28337654
Vasili Svirski <vasili.svirski@gmail.com>
2018-10-10 13:57:09 +0300

    Longest Valid Parentheses

---
+++ src/main/java/hard/Solutoon32.java
@@ -0,0 +1,26 @@
+package hard;
+
+import java.util.Stack;
+
+public class Solutoon32 {
+  public int longestValidParentheses(String s) {
+
+    int maxans = 0;
+    Stack<Integer> stack = new Stack<>();
+    stack.push(-1);
+
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) == '(') {
+        stack.push(i);
+      } else {
+        stack.pop();
+        if (stack.empty()) {
+          stack.push(i);
+        } else {
+          maxans = Math.max(maxans, i - stack.peek());
+        }
+      }
+    }
+    return maxans;
+  }
+}
