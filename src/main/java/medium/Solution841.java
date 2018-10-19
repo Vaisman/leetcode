b2628080e1d3c814665ddec632cd63633b87555c
Vasili Svirski <vasili.svirski@gmail.com>
2018-10-19 12:25:06 +0300

    Can Visit All Rooms

---
+++ src/main/java/medium/Solution841.java
@@ -0,0 +1,34 @@
+package medium;
+
+import java.util.List;
+import java.util.Stack;
+
+public class Solution841 {
+  public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+    boolean[] seen = new boolean[rooms.size()];
+    seen[0] = true;
+    Stack<Integer> stack = new Stack();
+    stack.push(0);
+
+    // At the beginning, we have a todo list "stack" of keys to use.
+    // 'seen' represents at some point we have entered this room.
+
+    while (!stack.isEmpty()) { // While we have keys...
+      int node = stack.pop(); // Get the next key 'node'
+      for (int nei : rooms.get(node)) {
+        if (!seen[nei]) { // ...that hasn't been used yet
+          seen[nei] = true; // mark that we've entered the room
+          stack.push(nei); // add the key to the todo list
+        }
+      }
+    }
+
+    for (boolean v : seen) { // if any room hasn't been visited, return false
+      if (!v) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
