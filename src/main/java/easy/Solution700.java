2b1bacc1cd6e21331c5da630d388a3362b543f10
Vasili Svirski <vasili.svirski@gmail.com>
2019-01-14 12:07:34 +0300

    Search in a Binary Search Tree

---
+++ src/main/java/easy/Solution700.java
@@ -0,0 +1,12 @@
+package easy;
+
+import common.TreeNode;
+
+public class Solution700 {
+    TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) return null;
+        if (root.val == val) return root;
+        if (root.val > val) return searchBST(root.left, val);
+        else return searchBST(root.right, val);
+    }
+}
---
+++ src/main/java/medium/Solution947.java
@@ -0,0 +1,9 @@
+package medium;
+
+public class Solution947 {
+    public int removeStones(int[][] stones) {
+      if (stones == null || stones.length ==0 || stones[0].length ==0)
+        return 0;
+      return 0;
+    }
+}
