96fbf44aa1a06557ea4ad44fd6bfc4c057d04780
Vasili Svirski <vasili.svirski@gmail.com>
2019-01-14 13:33:25 +0300

    Flipping an Image

---
+++ src/main/java/easy/Solution832.java
@@ -0,0 +1,12 @@
+package easy;
+
+public class Solution832 {
+    public int[][] flipAndInvertImage(int[][] A) {
+        int n = A.length;
+        for (int[] row : A)
+            for (int i = 0; i * 2 < n; i++)
+                if (row[i] == row[n - i - 1])
+                    row[i] = row[n - i - 1] ^= 1;
+        return A;
+    }
+}
