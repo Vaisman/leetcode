17d368a2a26783a24aa0e1cea4156881705a606b
Vasili Svirski <vasili.svirski@gmail.com>
2019-01-17 13:50:14 +0300

    Distance clearness

--- src/main/java/easy/Solution475.java
+++ src/main/java/easy/Solution475.java
@@ -18,10 +18,10 @@ public class Solution475 {
                 index = -(index + 1);
             }
 
-            int dist1 = index - 1 >= 0 ? house - heaters[index - 1] : Integer.MAX_VALUE;
-            int dist2 = index < heaters.length ? heaters[index] - house : Integer.MAX_VALUE;
+            int leftDist = index - 1 >= 0 ? house - heaters[index - 1] : Integer.MAX_VALUE;
+            int rightDist = index < heaters.length ? heaters[index] - house : Integer.MAX_VALUE;
 
-            result = Math.max(result, Math.min(dist1, dist2));
+            result = Math.max(result, Math.min(leftDist, rightDist));
         }
 
         return result;
