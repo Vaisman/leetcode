47e45343ec13127bbfa06f842ec542b3521abf27
Vasili Svirski <vasili.svirski@gmail.com>
2019-01-22 10:04:55 +0300

    N Queens count

---
+++ src/main/java/hard/Solution52.java
@@ -0,0 +1,46 @@
+package hard;
+
+import java.util.List;
+
+public class Solution52 {
+
+    int count = 0;
+    public int solveNQueens(int n) {
+        char[][] board = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+
+        dfs(board, 0);
+
+        return count;
+    }
+
+    private void dfs(char[][] board, int colIndex) {
+        if (colIndex == board.length) {
+            count++;
+            return;
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            if (validate(board, i, colIndex)) {
+                board[i][colIndex] = 'Q';
+                dfs(board, colIndex + 1);
+                board[i][colIndex] = '.';
+            }
+        }
+    }
+
+    private boolean validate(char[][] board, int x, int y) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < y; j++) {
+                if (board[i][j] == 'Q' && (x + j == y + i || x + y == i + j || x == i)) return false;
+            }
+        }
+
+        return true;
+    }
+}
