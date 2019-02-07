8b6aae03f3231b94471e265ff8a8171ae6fd10d8
Vasili Svirski <vasili.svirski@gmail.com>
2019-02-07 09:57:40 +0300

    Sort Array By Parity

---
+++ src/main/java/easy/Solution905.java
@@ -0,0 +1,18 @@
+package easy;
+
+public class Solution905 {
+    public int[] sortArrayByParity(int[] A) {
+        int[] ans = new int[A.length];
+        int t = 0;
+
+        for (int i = 0; i < A.length; ++i)
+            if (A[i] % 2 == 0)
+                ans[t++] = A[i];
+
+        for (int i = 0; i < A.length; ++i)
+            if (A[i] % 2 == 1)
+                ans[t++] = A[i];
+
+        return ans;
+    }
+}
