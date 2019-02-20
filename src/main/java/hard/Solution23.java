d8db28116b9654567d0c25aa900fb0cd2536c76c
Vasili Svirski <vasili.svirski@gmail.com>
2019-02-20 11:56:04 +0300

    Merge k Sorted Lists

---
+++ src/main/java/hard/Solution23.java
@@ -0,0 +1,31 @@
+package hard;
+
+public class Solution23 {
+    public static ListNode mergeKLists(ListNode[] lists) {
+        return partiton(lists, 0, lists.length - 1);
+    }
+
+    public static ListNode partiton(ListNode[] lists, int s, int e) {
+        if (s == e) return lists[s];
+        if (s < e) {
+            int q = (s + e) / 2;
+            ListNode l1 = partiton(lists, s, q);
+            ListNode l2 = partiton(lists, q + 1, e);
+            return merge(l1, l2);
+        } else
+            return null;
+    }
+
+    //This function is from Merge Two Sorted Lists.
+    public static ListNode merge(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = merge(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = merge(l1, l2.next);
+            return l2;
+        }
+    }
+}
